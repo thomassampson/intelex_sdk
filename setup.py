2766fd2f6291167cf0804dc9897d65554338bbf7
Thomas Sampson <sampsont91@gmail.com>
2019-01-06 20:06:37 -0500

    Bump version: 0.0.15 → 0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='intelex',
-    version='0.0.15',
+    version='0.0.16',
     description='Intelex SDK',
     url='https://github.com/thomassampson/intelex_sdk',
     author='Thomas Sampson',
--- tests/test_intelex.py
+++ tests/test_intelex.py
@@ -1,7 +1,7 @@
 from intelex import get_version, echo
 
 def test_get_version():
-    assert get_version() == '0.0.15'
+    assert get_version() == '0.0.16'
 
 def test_echo():
     assert echo('Test') == 'Test'
