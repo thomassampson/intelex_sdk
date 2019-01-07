6dfe99aab771b7bdecf352e41dd340b5cf9720fc
Thomas Sampson <sampsont91@gmail.com>
2019-01-06 21:47:54 -0500

    Added new functions

--- README.md
+++ README.md
@@ -8,7 +8,18 @@
 pip install intelex
 ```
 
+## PREREQUISITES
+
+You must set the following environment variables in order to use the SDK:
+
+1. **ilx_endpoint**: The endpoint of your intelex tenant. Example for if tenant/site is called 'Python': https://clients.intelex.com/Login3/Python
+2. **ilx_endpoint**: The api key for your intelex tenant. Example: dgH67SHvhGhsb7Jsbwe3HnsN6
+
 ## USAGE
+
+### Basic Functions
+
+Results are returned as a string.
 
 ```python
 import intelex as ilx
@@ -18,4 +29,30 @@ ilx.get_version()
 
 # Echo inputted parameter
 ilx.echo('Hello, World')
+
+```
+
+### Object Functions
+
+Results as returned as a dictionary.
+
+```python
+import intelex as ilx
+
+object_name = 'IncidentsObject'
+record_id = 'd0bf1ab9-6ff8-4362-8620-11cd74adb30a'
+related_name = 'SubIncidents'
+related_record_id = '3883f898-f6a3-40c8-be7e-8be000c596c6'
+
+# Returns all records from an Intelex object that the user is authorized to view
+ilx.get_records(object_name)
+
+# Returns an individual record from the Incidents object by referencing the UID of the record
+ilx.get_record(object_name, record_id)
+
+# Navigating to related records allows clients to request only the relational data belonging to a parent record.
+ilx.get_related_records(object_name, record_id, related_name):
+
+# Navigating to related records allows clients to request only specific relational data belonging to a parent record. 
+ilx.get_related_record(object_name, record_id, related_name, related_id)
 ```
--- src/intelex.py
+++ src/intelex.py
@@ -1,6 +1,67 @@
+import requests as r
+import os
+import json
+
+
 def get_version():
     return '0.0.18'
 
-def echo(value):
-    return value
-    
+
+def get_records(object):
+    ilx_endpoint = os.environ['ilx_endpoint']
+    ilx_apikey = os.environ['ilx_apikey']
+    auth_header = 'Basic {}'.format(ilx_apikey)
+
+    headers = {
+        'Authorization': auth_header
+    }
+
+    query = '{}/api/v2/object/{}'.format(ilx_endpoint, object)
+    response = r.get(query, headers=headers)
+
+    return json.loads(response.text)
+
+
+def get_record(object, id):
+    ilx_endpoint = os.environ['ilx_endpoint']
+    ilx_apikey = os.environ['ilx_apikey']
+    auth_header = 'Basic {}'.format(ilx_apikey)
+
+    headers = {
+        'Authorization': auth_header
+    }
+
+    query = '{}/api/v2/object/{}({})'.format(ilx_endpoint, object, id)
+    response = r.get(query, headers=headers)
+
+    return json.loads(response.text)
+
+
+def get_related_records(object, id, navigation_property):
+    ilx_endpoint = os.environ['ilx_endpoint']
+    ilx_apikey = os.environ['ilx_apikey']
+    auth_header = 'Basic {}'.format(ilx_apikey)
+
+    headers = {
+        'Authorization': auth_header
+    }
+
+    query = '{}/api/v2/object/{}({})/{}'.format(ilx_endpoint, object, id, navigation_property)
+    response = r.get(query, headers=headers)
+
+    return json.loads(response.text)
+
+
+def get_related_record(object, id, navigation_property, related_id):
+    ilx_endpoint = os.environ['ilx_endpoint']
+    ilx_apikey = os.environ['ilx_apikey']
+    auth_header = 'Basic {}'.format(ilx_apikey)
+
+    headers = {
+        'Authorization': auth_header
+    }
+
+    query = '{}/api/v2/object/{}({})/{}({})'.format(ilx_endpoint, object, id, navigation_property, related)
+    response = r.get(query, headers=headers)
+
+    return json.loads(response.text)
