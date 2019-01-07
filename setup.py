91ea6db064a245f98b987a9e09691b329ce56a2c
Thomas Sampson <sampsont91@gmail.com>
2019-01-06 19:26:48 -0500

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='intelex',
-    version='0.0.7',
+    version='0.0.8',
     description='Intelex SDK',
     url='https://github.com/thomassampson/intelex_sdk',
     author='Thomas Sampson',
